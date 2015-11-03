6759a3056fb4929f2b70ef07fb94965f4bc1a167
Cesar Chen <cesar6134@gmail.com>
2015-11-03 23:51:54 +0800

    Modified the module content in module03

--- hot_module_replacement/app/index.js
+++ hot_module_replacement/app/index.js
@@ -1,11 +1,15 @@
 import './styles.styl';
 let logTest = require('./module01');
+let htmlText = require('./module03');
 
 logTest['default']();
+document.getElementById('root').innerText = htmlText['default'];
 
 if (module.hot) {
-  module.hot.accept('./module01', () => {
+  module.hot.accept(['./module01', './module03'], () => {
     logTest = require('./module01');
+    htmlText = require('./module03');
     logTest['default']();
+    document.getElementById('root').innerText = htmlText['default'];
   });
 }
--- hot_module_replacement/app/module03.js
+++ hot_module_replacement/app/module03.js
@@ -1,1 +1,4 @@
 export const m3t1 = 'Anyone there?';
+
+const htmlText = 'Hello Orz!';
+export default htmlText;
